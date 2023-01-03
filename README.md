34c6f5d657c14a9436d7ecdf52634c0dbe9aaded
Poker <poker_sang@outlook.com>
2023-01-03 16:19:45 +0800

    update readme

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 Useful template methods for WinUI3 projects
 
-[![Nuget](https://img.shields.io/badge/Nuget-004880?&style=for-the-badge&logo=Nuget&logoColor=white)](https://www.nuget.org/packages/WinUI3Utilities)
+## Build
+
+![.NET](https://img.shields.io/badge/.Net-7.0-512BD4?&style=for-the-badge&logo=.NET&logoColor=white)
+![Windows](https://img.shields.io/badge/Windows-10.0.19041-512BD4?&style=for-the-badge&logo=Windows&logoColor=white)
+
+## Project Link 
+
+[![Nuget](https://img.shields.io/badge/Nuget-WinUI3Utilities-004880?&style=for-the-badge&logo=Nuget&logoColor=white)](https://www.nuget.org/packages/WinUI3Utilities)
+[![GitHub](https://img.shields.io/badge/GitHub-WinUI3Utilities-181717?&style=for-the-badge&logo=Github&logoColor=white)](https://github.com/Poker-sang/WinUI3Utilities)
+
+## Contact me
+
+[![Poker](https://img.shields.io/badge/Poker-poker__sang@outlook.com-0078D4?style=for-the-badge&logo=microsoft-outlook&logoColor=white)](mailto:poker_sang@outlook.com)
--- WinUI3Utilities/Attributes/LocalizedStringResourcesAttribute.cs
+++ WinUI3Utilities/Attributes/LocalizedStringResourcesAttribute.cs
@@ -9,11 +9,11 @@ namespace WinUI3Utilities.Attributes;
 /// <remarks>
 /// <seealso href="https://platform.uno/blog/using-msbuild-items-and-properties-in-c-9-source-generators/"/><br/>
 /// <strong>This Attribute Should Only Appear ONCE Per Project</strong><br/>
-/// To use this Attribute, first you should add this region to your csproj file
+/// To use this Attribute, at first, you should add this region to your .csproj file
 /// <code>
 /// &lt;<see langword="Target"/> Name="InjectAdditionalFiles" BeforeTargets="GenerateMSBuildEditorConfigFileShouldRun"&gt;
 ///     &lt;<see langword="ItemGroup"/>&gt;
-///         &lt;AdditionalFiles Include="@(PRIResource)" SourceItemGroup="PRIResource" /&gt;
+///         &lt;<see langword="AdditionalFiles"/> Include="@(PRIResource)" SourceItemGroup="PRIResource" /&gt;
 ///     &lt;/<see langword="ItemGroup"/>&gt;
 /// &lt;/<see langword="Target"/>&gt;
 /// &lt;<see langword="ItemGroup"/>&gt;
@@ -24,7 +24,7 @@ namespace WinUI3Utilities.Attributes;
 /// <code>
 /// &lt;<see langword="ItemGroup"/>&gt;
 ///     &lt;<see langword="PRIResource"/> Include="XXX\APage.resw" /&gt;
-///     &lt;<see langword="PRIResource"/> Include="XXX\BResource.resw" /&gt;
+///     &lt;<see langword="PRIResource"/> Include="XXX\BWindow.resw" /&gt;
 ///     ...
 /// &lt;/<see langword="ItemGroup"/>&gt;
 /// </code>
@@ -37,15 +37,15 @@ namespace WinUI3Utilities.Attributes;
 /// Code will generate:
 /// <code>
 /// <see langword="namespace"/> YourNamespace;<br/>
-/// <see langword="public static class"/> APage
+/// <see langword="public static class"/> APageResource
 /// {
-///     <see langword="private static readonly"/> <see cref="ResourceLoader"/> _resourceLoader = new(<see cref="ResourceLoader.GetDefaultResourceFilePath"/>, "APage");
+///     <see langword="private static readonly"/> <see cref="ResourceLoader"/> _resourceLoader = <see langword="new"/>(<see cref="ResourceLoader.GetDefaultResourceFilePath"/>, "APageResource");
 ///     <see langword="public static readonly string"/> TextAPath = _resourceLoader.GetString("TextA/Path");
 ///     <see langword="public static readonly string"/> TextBPath = _resourceLoader.GetString("TextB/Path");
 /// }<br/>
-/// <see langword="public static class"/> BResource
+/// <see langword="public static class"/> BWindowResource
 /// {
-///     <see langword="private static readonly"/> <see cref="ResourceLoader"/> _resourceLoader = new(<see cref="ResourceLoader.GetDefaultResourceFilePath"/>, "BResource");
+///     <see langword="private static readonly"/> <see cref="ResourceLoader"/> _resourceLoader = <see langword="new"/>(<see cref="ResourceLoader.GetDefaultResourceFilePath"/>, "BWindowResource");
 ///     <see langword="public static readonly string"/> ResourceAPath = _resourceLoader.GetString("ResourceA/Path");
 ///     <see langword="public static readonly string"/> ResourceBPath = _resourceLoader.GetString("ResourceB/Path");
 /// }
