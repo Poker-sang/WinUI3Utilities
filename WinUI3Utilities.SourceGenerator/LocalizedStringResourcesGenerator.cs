f96294f156e7dc7c5be090528ecde914e7ae49e4
Poker <poker_sang@outlook.com>
2025-02-09 20:38:40 +0800

    update

--- WinUI3Utilities.SourceGenerator/LocalizedStringResourcesGenerator.cs
+++ WinUI3Utilities.SourceGenerator/LocalizedStringResourcesGenerator.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Xml.Linq;
